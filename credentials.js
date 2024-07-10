b4a708e9f360bfbca44e953b7125f733376fe6a9
0z-syno <165568015+0z-syno@users.noreply.github.com>
2024-07-10 20:25:38 +0100

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,2 +1,5 @@
-const correctUsername = 'yourUsername';
-const correctPassword = 'yourPassword';
+const credentials = [
+    { username: 'user1', password: 'pass1' },
+    { username: 'user2', password: 'pass2' },
+    { username: 'user3', password: 'pass3' }
+];
